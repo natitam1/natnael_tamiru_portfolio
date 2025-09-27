18927e7e385b0f700f8e8470a5bbcd3f51004ed1
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-27 23:15:40 +0300

    feat: add animated blob SVG component

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { easeIn, easeInOut, motion } from "framer-motion";
+import Blob from "@/components/Blob";
 
 const Home = () => {
   return (
@@ -11,7 +12,15 @@ const Home = () => {
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
     >
-      home
+      <div className="h-screen flex items-center">
+        <div className="flex flex-col xl:flex-row items-center justify-between w-full">
+          <div className="w-full xl:w-[550px]">text</div>
+          <div className="hidden xl:block flex-1 relative z-20">
+            {/* blob */}
+            <Blob containerStyles="w-[560px] h-[560px]" />
+          </div>
+        </div>
+      </div>
     </motion.section>
   );
 };

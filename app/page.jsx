4c19f880ce42c5f5f78f6ef8c98f9dbc02a90038
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-28 14:26:27 +0300

    feat: add blob and an image for the hero section

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { easeIn, easeInOut, motion } from "framer-motion";
 import Blob from "@/components/Blob";
+import Image from "next/image";
+import avatarImg from "@/public/avatar5.png";
 
 const Home = () => {
   return (
@@ -18,6 +20,16 @@ const Home = () => {
           <div className="hidden xl:block flex-1 relative z-20">
             {/* blob */}
             <Blob containerStyles="w-[560px] h-[560px]" />
+            {/* avatar img */}
+            <Image
+              src={avatarImg}
+              width={440}
+              height={600}
+              quality={100}
+              className="absolute -top-22 left-[120px]"
+            />
+            {/* overlay gradient */}
+            <div className="w-full h-[164px] absolute bottom-0 left-0 right-0 bg-gradient-to-t from-primary via-primary/90 to-primary/40"></div>
           </div>
         </div>
       </div>
